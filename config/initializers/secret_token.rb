f23aca733b35d8e2a58b5b80e44465a00c6b8925
Douglas Teixeira <teixeir3@gmail.com>
2014-02-17 11:57:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PreventIT::Application.config.secret_token = 'fa1bc17cda71616d386308270220dead7933462f206b9c90379369110cfc57be8413e4d91177e0f35e703b85c9ce97bb42d903a43204802a9d830ab238c50fc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PreventIT::Application.config.session_store :cookie_store, key: '_PreventIT_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PreventIT::Application.config.session_store :active_record_store
