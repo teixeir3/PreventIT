1c4e096c3dfe65e27fd3ceba73562d5d39551cdd
Douglas Teixeira <teixeir3@gmail.com>
2014-02-24 12:10:15 -0500

    CSS: Changes sub-nav / logo image + tagline

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PreventIT::Application.config.secret_token = 'fa1bc17cda71616d386308270220dead7933462f206b9c90379369110cfc57be8413e4d91177e0f35e703b85c9ce97bb42d903a43204802a9d830ab238c50fc0'
+PreventIT::Application.config.secret_token = ENV["SECRET_TOKEN"]
