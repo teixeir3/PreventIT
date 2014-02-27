4a4da4c389016bfed2c39692b232ca088b15a5e7
App Academy Student <student@appacademy.io>
2014-02-27 18:41:58 -0500

    Changes omniauth.rb for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     {approval_prompt: '',
      access_type: 'offline',
      scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-     redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'}
+     redirect_uri:'http://damp-reaches-3345.herokuapp.com/auth/google_oauth2/callback'}
 end
