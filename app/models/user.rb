7ed003e0f395947e9b538aed313901532b6636e5
App Academy Student <student@appacademy.io>
2014-02-27 19:36:53 -0500

    Removes bug from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,6 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :session_token, presence: true, uniqueness: true
-  validates :uid
 
   before_validation :ensure_session_token
 
