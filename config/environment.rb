96f7903c6355b7659d91f07fae2f7d5abd4cf7c9
Douglas Teixeira <teixeir3@gmail.com>
2014-03-14 23:42:36 -0400

    Adds ActionMailer Sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PreventIT::Application.initialize!
+
+if Rails.env.production?
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method ||= :smtp
+elsif Rails.env.development?
+
+  ActionMailer::Base.delivery_method = :letter_opener
+end
