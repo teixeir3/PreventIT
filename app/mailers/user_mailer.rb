1943c2f6a3c4c86510b31fbd240a4351b67b2873
Douglas Teixeira <teixeir3@gmail.com>
2014-03-16 15:42:17 -0400

    Fixing bug in user_activation templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   
   def activation_email(user)
     @user = user
-   
+    @url = activate_users_url(activation_token: @user.activation_token)
     mail(to: user.email, subject: 'PreventIT: Activation Email')
   end
 end
