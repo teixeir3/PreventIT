5e1020587fced6604f40f2ed2751f0ac662f6354
Douglas Teixeira <teixeir3@gmail.com>
2014-05-10 15:47:45 -0500

    Adds error messages to password reset pages.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,16 +10,25 @@ class PasswordResetsController < ApplicationController
   
   def edit
     @user = User.find_by_password_reset_token!(params[:id])
+    
+    if @user.password_reset_sent_at < 24.hours.ago
+      flash.now[:errors] = ["Password reset has expired."]
+      render new_password_reset_path
+    end
   end
   
   def update
     @user = User.find_by_password_reset_token!(params[:id])
     
     if @user.password_reset_sent_at < 24.hours.ago
-      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+      flash.now[:errors] = ["Password reset has expired."]
+      render new_password_reset_path, :alert => "Password reset has expired."
     elsif @user.update_attributes(params[:user])
-      redirect_to root_url, :notice => "Password has been reset!"
+      sign_in(@user)
+      flash[:errors] = ["Password has been reset!"]
+      redirect_to user_url(@user), :notice => "Password has been reset!"
     else
+      flash.now[:errors] = @user.errors.full_messages
       render :edit
     end
   end
